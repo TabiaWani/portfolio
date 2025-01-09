6a5579abfaa8168fa669f5d59358716199db35dd
Shahid Ul Islam <65996596+Khanz9664@users.noreply.github.com>
2025-01-09 12:32:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,27 @@ document.addEventListener("DOMContentLoaded", () => {
     hamburger.addEventListener("click", () => {
         navLinks.classList.toggle("show");
     });
+
+    // Smooth Scrolling
+    const sections = document.querySelectorAll('section[id]');
+    const navLinks = document.querySelectorAll('header .nav-links a');
+
+    window.addEventListener('scroll', () => {
+        let current = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (scrollY >= (sectionTop - sectionHeight / 3)) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        navLinks.forEach(a => {
+            a.classList.remove('active');
+            if (a.classList.contains(current)) {
+                a.classList.add('active');
+            }
+        });
+    });
 });
